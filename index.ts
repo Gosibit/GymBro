e20921630075917960a4a354f2f9ddd5c3b6cee2
Kamil Sawicki <gosibi6@gmail.com>
2022-08-31 00:00:10 +0200

    dass

--- index.ts
+++ index.ts
@@ -14,19 +14,19 @@ app.use('/public', express.static(path.join(__dirname, 'public')))
 
 dotenv.config()
 const port = process.env.PORT || 3000
-// const dbURI = `mongodb+srv://${process.env.MONGODB_USER_LOGIN}:${process.env.MONGODB_USER_PASSWORD}@gymbro.ablfct2.mongodb.net/${process.env.DATABASE_NAME}?retryWrites=true&w=majority`
+const dbURI = `mongodb+srv://${process.env.MONGODB_USER_LOGIN}:${process.env.MONGODB_USER_PASSWORD}@gymbro.ablfct2.mongodb.net/${process.env.DATABASE_NAME}?retryWrites=true&w=majority`
 ;(async () => {
-    // try {
-    // await mongoose.connect(dbURI, {})
-    app.listen(port, () => console.log(`Listening on port ${port}`))
-    //     app.use((req: express.Request, res: express.Response, next: express.NextFunction) => {
-    //         next()
-    //     })
-    //     app.use('/public', express.static(__dirname + '/public'))
-    //     app.use(bodyParser.json())
-    //     app.use(bodyParser.urlencoded({ extended: true }))
-    //     app.use('/users', userRoutes)
-    //     app.use('/auth/', authRoutes)
-    //     app.use('/products/', productRoutes)
-    // } catch (error) {}
+    try {
+        await mongoose.connect(dbURI, {})
+        app.listen(port, () => console.log(`Listening on port ${port}`))
+        app.use((req: express.Request, res: express.Response, next: express.NextFunction) => {
+            next()
+        })
+        app.use('/public', express.static(__dirname + '/public'))
+        app.use(bodyParser.json())
+        app.use(bodyParser.urlencoded({ extended: true }))
+        app.use('/users', userRoutes)
+        app.use('/auth/', authRoutes)
+        app.use('/products/', productRoutes)
+    } catch (error) {}
 })()
