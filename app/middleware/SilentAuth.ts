53237a7dca8dc2012730f67fb1dd60c8ead41f5e
Kamil Sawicki <gosibi6@gmail.com>
2022-09-05 20:33:00 +0200

    new middleware SilentAuth

---
+++ app/middleware/SilentAuth.ts
@@ -0,0 +1,24 @@
+import express from 'express'
+import jwt from 'jsonwebtoken'
+import User from '../models/User'
+
+const SilentAuth = function (
+    req: express.Request,
+    res: express.Response,
+    next: express.NextFunction
+) {
+    if (!process.env.ACCESS_TOKEN_SECRET) throw Error('NO ACCESS TOKEN SECRET')
+    const authHeader = req.headers['authorization']
+    const token = authHeader && authHeader.split(' ')[1]
+    if (!token) return next()
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, async (err, payload: any) => {
+        if (err) throw Error('Authentication Failed')
+        const user = await User.findById(payload._id).orFail()
+        if (user.passwordChangedDate > payload.iat * 1000)
+            return res.status(401).json('Token expired')
+        req.user = user
+        next()
+    })
+}
+
+export default SilentAuth
