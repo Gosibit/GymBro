44808a3ad962f2924d8e18f2eb673cf2b271a3cd
Kamil Sawicki <gosibi6@gmail.com>
2022-09-09 14:32:18 +0200

    shopping cart fix

--- app/controllers/ShoppingCartsController.ts
+++ app/controllers/ShoppingCartsController.ts
@@ -11,25 +11,21 @@ function findProductIndex(cart: IShoppingCart, productId: string, size: string) 
         (SCproduct) => SCproduct.product._id.toString() === productId && SCproduct.size === size
     )
 }
-
 class ShoppingCartsController {
     public async getCart(req: express.Request, res: express.Response) {
         try {
-            if (!req.params.shoppingCartId && !req.user) throw Error('No shopping cart id')
+        
+            if (!req.query.shoppingCartId && !req.user) throw Error('No shopping cart id')
 
             let cart: IShoppingCart
 
             if (req.user) {
                 cart = await cartQuery({ user: req.user._id })
             } else {
-                cart = await cartQuery({ _id: req.params.shoppingCartId })
+                cart = await cartQuery({ _id: req.query.shoppingCartId })
             }
-
-            const total = cart.products.reduce((acc, SCproduct) => {
-                // sc = shopping cart
-                return acc + SCproduct.product.price * SCproduct.quantity
-            }, 0)
-            return res.status(200).json({ ...cart.toJSON(), total })
+            await cart.save()
+            return res.status(200).json(cart)
         } catch (error) {
             console.log(error)
             return res.status(422).json({ message: 'There was an error while getting cart' })
@@ -39,30 +35,32 @@ class ShoppingCartsController {
         try {
             let cart: IShoppingCart
 
-            if (!req.params.shoppingCartId && !req.user) {
+            if (!req.body.shoppingCartId && !req.user) {
                 cart = await ShoppingCart.create({ products: [] })
-                cart = await cart.populate('products.product')
             } else if (req.user) {
                 cart = await cartQuery({ user: req.user._id })
             } else {
-                cart = await cartQuery({ _id: req.params.shoppingCartId })
+                cart = await cartQuery({ _id: req.body.shoppingCartId })
             }
-
+            
             const { productId, quantity, size } = req.body
             if (!productId || !quantity || parseInt(quantity) != quantity || !size) {
                 throw Error('No product id or quantity or size')
             }
-
+            
             const productIndex = findProductIndex(cart, productId, size)
             if (productIndex === -1) {
                 cart.products.push({ product: productId, quantity: parseInt(quantity), size: size })
             } else {
                 cart.products[productIndex].quantity += parseInt(quantity)
             }
-
+            
             await cart.save()
+            
             const populatedCart = await cartQuery({ _id: cart._id })
 
+            await populatedCart.save()
+            
             return res.status(200).json(populatedCart)
         } catch (error) {
             console.log(error)
@@ -71,11 +69,11 @@ class ShoppingCartsController {
     }
     public async removeFromCart(req: express.Request, res: express.Response) {
         try {
-            if (!req.params.shoppingCartId && !req.user) throw Error('No shopping cart id')
+            if (!req.query.shoppingCartId && !req.user) throw Error('No shopping cart id')
 
             let cart: IShoppingCart
             if (req.user) cart = await cartQuery({ user: req.user._id })
-            else cart = await cartQuery({ _id: req.params.shoppingCartId })
+            else cart = await cartQuery({ _id: req.query.shoppingCartId })
 
             const { productId, size } = <any>req.query
             if (!productId || !size) throw Error('No product id or size')
@@ -93,12 +91,12 @@ class ShoppingCartsController {
     }
     public async updateCart(req: express.Request, res: express.Response) {
         try {
-            if (!req.params.shoppingCartId && !req.user) throw Error('No shopping cart id')
+            if (!req.body.shoppingCartId && !req.user) throw Error('No shopping cart id')
 
             let cart: IShoppingCart
 
             if (req.user) cart = await cartQuery({ user: req.user._id })
-            else cart = await cartQuery({ _id: req.params.shoppingCartId })
+            else cart = await cartQuery({ _id: req.body.shoppingCartId })
 
             const { productId, quantity, size } = req.body
             if (!productId || !quantity || parseInt(quantity) != quantity || !size)
